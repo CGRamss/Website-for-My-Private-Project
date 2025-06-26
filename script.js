9720e2da74fa4e5042bdb7fbb68f56ff40ae5560
CGRamss <irfanramadhan748@gmail.com>
2025-06-26 22:11:42 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// let currentValue = 1;
+
+// function toggleValue() {
+//     currentValue = currentValue === 1 ? 0 : 1;
+//     document.getElementById("value").textContent = currentValue;
+//     // Simpan nilai terbaru ke file (untuk GitHub Pages, kita gunakan trik)
+//     localStorage.setItem('controlValue', currentValue);
+// }
